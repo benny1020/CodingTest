dafee6d2b74ca3723d5f7679f63691cacfea0128
benny1020 <benny1020@naver.com>
2021-06-27 21:59:36 +0900

    210627

---
+++ prob7_BestAlbum.cpp
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42579
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+    q
+}
--- prob7_BestAlbum.py
+++ prob7_BestAlbum.py
@@ -1,3 +1,7 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42579
+
+
+
 a = []
 
 
