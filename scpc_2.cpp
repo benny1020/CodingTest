abbafd2ace1cae0d6a54903c232329d589486720
benny1020 <benny1020@naver.com>
2021-07-16 22:59:23 +0900

    210716

---
+++ scpc_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int test_case;
+    int len;
+    int t;
+    cin >> test_case;
+    for(int i=0;i<test_case;i++)
+    {
+        
+    }
+
+}
