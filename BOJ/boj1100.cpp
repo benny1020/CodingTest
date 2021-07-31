38ccd9f09f3c216c0deab251d7aab30b3b0971fb
benny1020 <benny1020@naver.com>
2021-07-31 22:12:22 +0900

    210731

---
+++ BOJ/boj1100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char chess[8][8];
+    for(int i=0;i<8;i++)
+        for(int j=0;j<8;j++)
+        {
+            cin >> chess[i][j];
+        }
+}
