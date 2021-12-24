bc48a67960a2570a4f50baa174c61daf9ae818a3
benny1020 <benny1020@naver.com>
2021-12-24 23:59:20 +0900

    211224

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-
-using namespace std;
-
+#include <stdio.h>
 
 int main(void)
 {
-	vector<vector<int>> st = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]];
-	cout << st[0].size() <<endl;
+	for(int i=1;i<=245;i++)
+	{
+		if(245%i == 0)
+			printf("%d ", i);
+	}
+	printf("\n");
 	return 0;
 }
