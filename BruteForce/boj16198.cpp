50c7b2c5134c30233754440686cae237edc29bf9
benny1020 <benny1020@naver.com>
2022-02-10 13:13:33 +0900

    baekjoon 9663 n-queen

--- BruteForce/boj16198.cpp
+++ BruteForce/boj16198.cpp
@@ -2,7 +2,26 @@
 #include <vector>
 
 using namespace std;
+// 준비 - 호출 - 복원
+// 아래와 같이 새로운 벡터 배열을 만들어서 복사한후 그것을 인자로 전달한다.
 
+int go(vector<int> &a) {
+  int n = a.size();
+  if (n == 2)
+    return 0;
+  int ans = 0;
+  for (int i = 1; i < n - 1; i++) {
+    int energy = a[i - 1] * a[i + 1];
+    vector<int> b(a);
+    b.erase(b.begin() + i);
+    energy += go(b);
+    if (ans < energy) {
+      ans = energy;
+    }
+  }
+  return ans;
+}
+/*  내가 짠 코드
 int go(int score,int n, vector<int>arr)
 {
     if(n==2)
@@ -19,15 +38,15 @@ int go(int score,int n, vector<int>arr)
     }
     return ans;
 }
+*/
 
 int main(void) {
-    int n;
-    cin >> n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++)
-        cin >> arr[i];
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
 
-    cout << go(0,n,arr) << endl;
-    return 0;
-
+  cout << go(arr) << endl;
+  return 0;
 }
