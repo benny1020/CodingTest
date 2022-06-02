7aeb372ecee9c4ec51c2039b74e1253a4a236caa
benny1020 <benny1020@naver.com>
2022-06-02 23:29:59 +0900

    update

--- cpp-study/baekjoon/boj17427.cpp
+++ cpp-study/baekjoon/boj17427.cpp
@@ -4,12 +4,25 @@ using namespace std;
 
 int main(void) {
     long long int n;
-    cin >> n;
-    long long int total=0;
-    for(long long int i=1;i<=n;i++){
-        total+=i*(n/i);
+    int k;
+    cin >>k;
+    int dp[1000000]={0,};
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int j=0;j<k;j++) {
+        cin >> n;
+        long long int total=0;
+        for(long long int i=1;i<=n;i++){
+            total+=i*(n/i);
+        }
+        cout << total <<endl;
     }
-    cout << total <<endl;
+
+8
+1 2 4 8
+
+16
+1 2 4 8 16
     return 0;
 
 }
