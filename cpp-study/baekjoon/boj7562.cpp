fd1c042235e2cd07d4f838904d486de98706b2ef
benny1020 <benny1020@naver.com>
2022-06-13 19:03:11 +0900

    boj 7562

---
+++ cpp-study/baekjoon/boj7562.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int bfs(int n, pair<int> cur, pair<int> target) {
+    queue<pair<pair<int,int> ,int > q;
+    
+    int count;
+}
+
+int main(void) {
+    int n;
+    
+}
